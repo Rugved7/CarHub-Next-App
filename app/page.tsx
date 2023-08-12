75165a3aee8fff3a6860b55a917df74778f1f16b
Rugved <rugvedagasti7@gmail.com>
2023-08-12 12:34:15 +0530

    Done with All the changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { Car, Filter, Hero, Search } from "@/components";
-import Image from "next/image";
+
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
+export default async function Home({searchParams}) {
   // importing the results form the API
 
-  const allCars = await fetchCars();
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit:searchParams.limit || 10,
+    model:searchParams.model || '',
+  });
   // before fetching the data , we need to know that data is empty
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -19,19 +25,6 @@ export default async function Home() {
           <h1 className="text-4xl">Cars Catalogue</h1>
           <p>Explore the cars you might desire...</p>
         </div>
-
-        {/* Create The Filters */}
-        <div className="home__filter">
-          {/* Include the Search component for the search bar */}
-          <Search />
-
-          <div className="home__filter-container">
-            {/* Ṭitle is passed as props here */}
-            <Filter title="fuel" />
-            <Filter title="year" />
-          </div>
-        </div>
-
         {/* Fetching the Actual data */}
         {!isDataEmpty ? (
           <section> 
